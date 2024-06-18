69b40a17d35d1df135efcbb438cbc298715d7c30
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-18 10:53:19 -0300

    Update ej5.R

--- S1U4/Ejercicios/ej5.R
+++ S1U4/Ejercicios/ej5.R
@@ -1,22 +1,22 @@
 #Para estos ejercicios utilizaremos las alturas recolectadas por Francis Galton
-#Para sus estudios genéticos, en este caso la de los niños
+#Para sus estudios genÃ©ticos, en este caso la de los niÃ±os
 library(HistData)
 data(Galton)
 x <- Galton$child
-#En los ejercicios anteriores vimos que el valor medio y promedio
-#son muy similares como la desviación estándar y MAD.
-#Esto es esperable ya que los datos son aproximables por una distribución estándar.
+#En los ejercicios anteriores vimos que el valor medio (median) y promedio (mean)
+#son muy similares como la desviaciÃ³n estÃ¡ndar (sd) y MAD.
+#Esto es esperable ya que los datos son aproximables por una distribuciÃ³n estÃ¡ndar.
 
-#Supongamos ahora que Galton cometió un error en el ingreso de los datos
+#Supongamos ahora que Galton cometiÃ³ un error en el ingreso de los datos
 #Que en el primer valor olvido utilizar el . para separar los decimales.
 #Podemos simular este error ingresando:
 x_with_error <- x
 x_with_error[1] <- x_with_error[1]*10
-#Obteniendo entonces valores diferentes en la primera posición del vector
+#Obteniendo entonces valores diferentes en la primera posiciÃ³n del vector
 x_with_error[1]
 x[1]
-#La aproximación normal no tendrá en cuenta este valor.
+#La aproximaciÃ³n normal no tendrÃ¡ en cuenta este valor.
 #Veremos como este valor anormal afecta el promedio.
-#Reporta cuantas inches el promedio creció luego de este error
-#Específicamente, reporta la diferencia entre el promedio de los datos
+#Reporta cuantas inches el promedio creciÃ³ luego de este error
+#EspecÃ­ficamente, reporta la diferencia entre el promedio de los datos
 #con el error 'x_with_error' y los sin el error 'x'

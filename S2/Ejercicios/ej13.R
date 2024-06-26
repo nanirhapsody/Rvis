08d133380242fae0b31c865dbb0753950cccad22
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:20:29 -0300

    Update ej13.R

--- S2/Ejercicios/ej13.R
+++ S2/Ejercicios/ej13.R
@@ -1,15 +1,15 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(murders)
 
-#Guardamos y creamos un objeto p que almacene el grafico que hemos
+#Guardamos y creamos un objeto p que almacene el gr√°fico que hemos
 #creado hasta ahora
 p <- murders %>% ggplot(aes(population, total, label = abb, color = region)) +
   geom_label() 
-#Cambia ambos ejes x e y a escalas logarÌtmicas
-#recuerda la funciÛn scale_x_log10() que vimos en el teÛrico
-#Agrega ambas capas en las siguientes lÌneas
+#Cambia ambos ejes x e y a escalas logar√≠tmicas
+#recuerda la funci√≥n scale_x_log10() que vimos en el te√≥rico
+#Agrega ambas capas en las siguientes l√≠neas:

075d26e55afc566e89ca4543963c399c5d4ce7f4
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:08:50 -0300

    Update ej7.R

--- S2/Ejercicios/ej7.R
+++ S2/Ejercicios/ej7.R
@@ -1,22 +1,22 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librería tidyverse
+#se encuentran en la librerÃ­a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(murders)
 
 #si queremos agregar texto a los puntos del
-#grafico podemos utilizar las funciones geométricas
+#grafico podemos utilizar las funciones geomÃ©tricas
 #geom_text() o geom_label()
 #Pero si ejecutamos lo siguiente
 murders %>% ggplot(aes(population, total)) +
   geom_label()
 #Notamos un mensaje de error
-#Selecciona la opción que indica a que se debe el error
+#Identifica la opciÃ³n que indica a que se debe el error:
 #op1) Necesitamos mapear cada caracter a su punto
 #mediante el argumento aes
 #op2) Necesitamos indicarle a geom_label que tipo de caracter utilizar
-#op3) La función geom_label no requiere valores para
+#op3) La funciÃ³n geom_label no requiere valores para
 #el eje x y eje y
-#geom_label no es un comando de la librería ggplot2
+#geom_label no es un comando de la librerÃ­a ggplot2

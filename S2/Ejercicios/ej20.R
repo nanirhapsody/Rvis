2959adf5134386928f23d0877225747861c332d7
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:26:09 -0300

    Update ej20.R

--- S2/Ejercicios/ej20.R
+++ S2/Ejercicios/ej20.R
@@ -1,13 +1,13 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(heights)
 
 
-#Ahora queremos realizar dos gr·ficos de densidad uno para hombres y otro para mujeres
-#debes entonces mapear asociando el sexo a cada grafico
+#Ahora queremos realizar dos gr√°ficos de densidad uno para hombres y otro para mujeres
+#debes entonces mapear asociando el sexo a cada gr√°fico
 #para asociar y agrupar por sexo puedes utilizar el argumento group dentro
-#dentro de la funciÛn aes
+#dentro de la funci√≥n 'aes'

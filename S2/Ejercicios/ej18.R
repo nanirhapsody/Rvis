dadf15a5ae76e8709c7294aa1ce10aee64167e59
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:24:55 -0300

    Update ej18.R

--- S2/Ejercicios/ej18.R
+++ S2/Ejercicios/ej18.R
@@ -1,14 +1,16 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(heights)
 
 
-#En el ejercicio anterior vemos que al hacer el grafico obtenemos un warning:
+#En el ejercicio anterior vemos que al realizar el gr√°fico obtenemos un warning:
 #stat_bin() using bins = 30. Pick better value with binwidth.
-#utiliza el argumento binwidth para cambiar el tamaÒo de los bins o 
-#barritas a 1 inch. Agrega entonces la capa geom_histogram pero especificando
-#el par·metro de entrada binwidth.
+#utiliza el argumento 'binwidth' para cambiar el tama√±o de los bins o 
+#barritas a 1 inch. 
+
+#Agrega entonces la capa 'geom_histogram' pero ahora especificando
+#el par√°metro de entrada 'binwidth':

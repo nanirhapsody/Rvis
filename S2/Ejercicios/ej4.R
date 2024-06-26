c92e5abe3ec6ae1f65f1954047d253d115fe4063
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:05:50 -0300

    Update ej4.R

--- S2/Ejercicios/ej4.R
+++ S2/Ejercicios/ej4.R
@@ -6,14 +6,12 @@ library(ggplot2)
 library(dslabs)
 data(murders)
 
-#Explorar el data frame de murders e identifica las
-#variables total de asesinatos y población
-#de manera de poder graficarlas en los siguientes ejercicios
-#Marca el nombre correcto de estas variables
+#Explorar el data frame de murders e identificar las
+#variables de: 'total de asesinatos' y 'poblaciÃ³n'
+#de manera de poder graficarlas en los siguientes ejercicios.
+
+#Encuentra el nombre correcto de estas variables:
 #op1)state and abb
-
 #op2)total_murders y population_size
-
 #op3)total y population
-
 #op4)murders y size

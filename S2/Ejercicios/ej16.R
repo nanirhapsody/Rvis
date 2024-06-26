f341486c1d5dcc12212f5412e261a23ee6f85e39
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:22:22 -0300

    Update ej16.R

--- S2/Ejercicios/ej16.R
+++ S2/Ejercicios/ej16.R
@@ -1,11 +1,11 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(heights)
 
 
-#Crea un objeto ggplot y utilizando el pipe asÌgnale los datos de alturas
-#Asigna la altura al eje x del gr·fico con la funciÛn de mapeo aes
+#Crea un objeto 'ggplot' y utilizando el pipe as√≠gnale los datos de alturas
+#Asigna la altura al eje x del gr√°fico con la funci√≥n de mapeo 'aes'

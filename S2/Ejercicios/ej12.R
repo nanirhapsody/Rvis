de87d603b0d5f05fecaf435753ce7c35a70fd3bd
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:19:57 -0300

    Update ej12.R

--- S2/Ejercicios/ej12.R
+++ S2/Ejercicios/ej12.R
@@ -1,17 +1,17 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(murders)
 
-#Previamente utilizamos este cÛdigo para realizar un gr·fico con los estados
+#Previamente utilizamos este c√≥digo para realizar un gr√°fico con los estados
 #abreviados como textos o labels
 murders %>% ggplot(aes(population, total, label = abb)) +
   geom_label()
-#ModifÌcalo haciendo que el color correspondiente a cada texto sea 
-#asociado a la regiÛn en que se encuentra el estado.
-#Como esto es un mapeo necesitarÌas utilizar la funciÛn aes.
-#Como ya est· definida la funciÛn aes dentro del ggplot
-#utiliza esta.
+#Modif√≠calo haciendo que el color correspondiente a cada texto sea 
+#asociado a la regi√≥n en que se encuentra el estado.
+#Como esto es un mapeo necesitar√°s utilizar la funci√≥n 'aes'.
+#Como ya est√° definida la funci√≥n 'aes' dentro del ggplot
+#utiliza esta y no la redefinas.

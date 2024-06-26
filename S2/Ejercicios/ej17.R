7b013e9bd2501a4aa6f11de21bf70db9604a1d6b
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:23:32 -0300

    Update ej17.R

--- S2/Ejercicios/ej17.R
+++ S2/Ejercicios/ej17.R
@@ -1,14 +1,14 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(heights)
 
 
-#En el ejercicio anterior creamos el objeto para el grafico
+#En el ejercicio anterior creamos el objeto 'ggplot' necesario para realizar un gr√°fico:
 p <- heights %>% 
   ggplot(aes(height))
 #Agrega ahora una capa que grafique el histograma del objeto anterior p
-#Recuerda la funciÛn geom_histogram
+#Recuerda la funci√≥n 'geom_histogram'

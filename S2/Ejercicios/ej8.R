c5027b652323c2e63c1ded913dcea206ce6280df
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:11:24 -0300

    Update ej8.R

--- S2/Ejercicios/ej8.R
+++ S2/Ejercicios/ej8.R
@@ -1,13 +1,14 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librerÌa tidyverse
+#se encuentran en la librer√≠a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(murders)
 
-#Edita la siguiente lÌnea para agregar un texto
-#a cada punto que sea la abreviaciÛn del estado
-## utiliza la funciÛn geom_label en lugar de geom_point
+#Edita la siguiente l√≠nea para agregar un texto
+#a cada punto que sea la abreviaci√≥n del estado
+## utiliza la funci√≥n geom_label en lugar de geom_point
 murders %>% ggplot(aes(population, total)) +
   geom_point()
+#Prueba tambi√©n la funci√≥n geomtext() para agregar estos textos al gr√°fico.

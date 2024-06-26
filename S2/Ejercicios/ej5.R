7380b4e497041344a630198333b6856fb62fda21
nanirhapsody <38499893+nanirhapsody@users.noreply.github.com>
2024-06-26 12:06:59 -0300

    Update ej5.R

--- S2/Ejercicios/ej5.R
+++ S2/Ejercicios/ej5.R
@@ -1,14 +1,14 @@
 #Cargamos los datos de asesinatos
 #Recordar que ambos paquetes ggplot2 y dplyr 
-#se encuentran en la librería tidyverse
+#se encuentran en la librerÃ­a tidyverse
 library(dplyr)
 library(ggplot2)
 library(dslabs)
 data(murders)
 
-#Para graficar un scatter plot (diagrama de dispersión)
-#Creamos una capa con la función geom_point
+#Para graficar un scatter plot (diagrama de dispersiÃ³n)
+#Creamos una capa con la funciÃ³n geom_point
 #Y luego mapeamos el eje x e y con el comando aes
-## Llena los blancos con las variables a graficar
+#En el siguiente cÃ³digo llena los blancos con los nombres de las variables a graficar:
 murders %>% ggplot(aes(x = , y = )) +
   geom_point()
